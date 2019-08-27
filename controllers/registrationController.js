0667a2001c930e0c63023a4181fc2267c3d2477c
pdahal1 <dahalpar@gmail.com>
2019-08-27 00:06:18 -0700

    Hashing the password

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 var router = express.Router();
 var ObjectId = require('mongoose').Types.ObjectId;
 
-var {Registration} = require('../models/registration');
+var { Registration } = require('../models/registration');
 
 router.get('/', (req, res) => {
   Registration.find((err, doc) => {
@@ -15,62 +16,69 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+
 router.post('/', (req, res) => {
   var reg = new Registration({
-    firstName: req.body.firstName, 
-    lastName: req.body.lastName, 
-    phone: req.body.phone, 
-    email: req.body.email, 
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
     password: req.body.password
-   
   });
-  reg.save((err, doc) => {
-    if (!err) {
-      res.send(doc);
-    } else {
-      console.log(
-        'error in saving the User object' + JSON.stringify(err, undefined, 2)
-      );
-    }
+
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(reg.password, salt, function (err, hash) {
+      if (err) {
+        console.log(err);
+      }
+      reg.password = hash;
+      reg.save((err, doc) => {
+        if (!err) {
+          res.send(doc);
+        } else {
+          console.log('error in saving the user object' + JSON.stringify(err, undefined, 2));
+        }
+      });
+    });
   });
 });
 
 
+router.post('/login', (req, res) => {
+  let userData = req.body;
+  Registration.findOne({ email: userData.email }, (error, user) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (!user) {
+        res.status(401).send('invalid email')
+      } else {
+        if (user.password !== userData.password) {
+          res.status(401).send('invalid password');
+        } else
+          res.status(200).send(user);
+      }
+    }
+  });
+});
+
+module.exports = router; 
+
 //following code will add the events tese items are hardcoaded but they can be fetched from 
 // the database too. 
- router.get('/events', (req, res)=>{
-   let events = [
-    {"_id" : "3", 
-    "name" :  "auto expo"} ,
+//  router.get('/events', (req, res)=>{
+//    let events = [
+//     {"_id" : "3", 
+//     "name" :  "auto expo"} ,
 
-    {"_id" : "4", 
-    "name" :  "auto expo"} 
-  ]
-  res.json.events
-   
+//     {"_id" : "4", 
+//     "name" :  "auto expo"} 
+//   ]
+//   res.json.events
 
- }); 
 
-// router.post('/login', (req, res) => {
-//   let userData = req.body;
-
-//   Registration.findOne({ email: userData.email }, (error, user) => {
-//       if (error) {
-//           console.log(error);
-//       } else {
-//           if (!user) {
-//               res.status(401).send('invalid email')
-//           } else {
-//               if (user.password !== userData.password) {
-//                   res.status(401).send('invalid password');
-//               } else 
-//                   res.status(200).send(user);
-              
-//           }
-//       }
-//   });
-
-// }); 
+//  });
 
 // router.get('/:id', (req, res) => {
 //   if (!ObjectId.isValid(req.params.id))
@@ -124,7 +132,7 @@ router.post('/', (req, res) => {
 // router.delete('/:id', (req, res) => {
 //   if (!ObjectId.isValid(req.params.id))
 //     return res.status(400).send(`cannot delete the item with the id: ${req.params.id}`);
-  
+
 //   User.findByIdAndRemove(req.params.id, (err, docs) => {
 //     if (!err) {
 //       res.send(docs);
@@ -134,4 +142,4 @@ router.post('/', (req, res) => {
 //   });
 // });
 
-module.exports = router; 
+
